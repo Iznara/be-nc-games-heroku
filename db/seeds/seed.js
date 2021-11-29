866c8b158b50338c247235450d7ba9e5b951db8a
Iznara <ryanmcguire1991@gmail.com>
2021-11-29 16:16:21 +0000

    fixed syntax errors and data types + installed pg-format dependency

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,4 +1,5 @@
 const db = require('../connection.js');
+const format = require('pg-format');
 
 const seed = async (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
@@ -12,21 +13,21 @@ const seed = async (data) => {
   await db.query(`
   CREATE TABLE categories (
   slug VARCHAR PRIMARY KEY,
-  description VARCHAR(MAX));`);
+  description TEXT);`);
 
   await db.query(`
   CREATE TABLE users (
   username VARCHAR PRIMARY KEY UNIQUE,
-  avatar_url VARCHAR(MAX),
+  avatar_url TEXT,
   name VARCHAR NOT NULL);`);
 
   await db.query(`
   CREATE TABLE reviews (
   review_id SERIAL PRIMARY KEY,
   title VARCHAR NOT NULL,
-  review_body VARCHAR(MAX),
+  review_body TEXT,
   designer VARCHAR,
-  review_img_url VARCHAR(MAX)
+  review_img_url TEXT
   DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
   votes INT DEFAULT 0,
   category VARCHAR REFERENCES categories(slug) ON DELETE SET NULL,
@@ -37,9 +38,10 @@ const seed = async (data) => {
   CREATE TABLE comments (
   comment_id SERIAL PRIMARY KEY,
   author VARCHAR REFERENCES users(username) ON DELETE SET NULL,
-  review_id VARCHAR REFERENCES reviews(review_id) ON DELETE CASCADE,
+  review_id INT REFERENCES reviews(review_id) ON DELETE CASCADE,
   votes INT DEFAULT 0,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`);
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  body TEXT NOT NULL);`);
 
   // 2. insert data
   const qryStr1 = format(`
