7bee5250dde871a77b5611fc719601120bf3cfe5
Iznara <ryanmcguire1991@gmail.com>
2021-11-30 13:01:01 +0000

    routing setup for /api/categories endpoint

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const apiRouter = require('./routes/api.router');
+//const { handleCustomErrors, handlePsqlErrors, handleServerErrors } = require("./errors/errorHandling");
+
+const app = express();
+app.use(express.json());
+
+app.use('/api', apiRouter);
+
+// app.all('*', (req, res) => {
+//   res.status(404).send({ msg: 'Route not found' });
+// });
+
+// app.use(handleCustomErrors);
+// app.use(handlePsqlErrors);
+// app.use(handleServerErrors);
+
+module.exports = app;
