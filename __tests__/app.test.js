f5cbad89bc1dfefcdaae4352c5d0979200e19042
Iznara <ryanmcguire1991@gmail.com>
2021-11-30 16:39:58 +0000

    global check for 404 errors testes and implemented

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,24 +9,39 @@ beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
 
-describe('GET /api/categories', () => {
-    test('status:200, responds with an array of \'categories\' objects', () => {
+describe('app handles all invalid urls', () => {
+    test('status:404 invalid route', () => {
         return request(app)
-            .get('/api/categories')
-            .expect(200)
+            .get('/vdfvdfv/svfvdfv')
+            .expect(404)
             .then(({ body }) => {
-                expect(body.categories).toBeInstanceOf(Array);
-                expect(body.categories).toHaveLength(4);
-                body.categories.forEach((category) => {
-                    expect(category).toEqual(
-                        expect.objectContaining({
-                            slug: expect.any(String),
-                            description: expect.any(String)
-                        }));
-                });
+                expect(body.msg).toBe('Route not found');
             });
     });
+
+
+
+    describe('GET /api/categories', () => {
+        test('status:200, responds with an array of \'categories\' objects', () => {
+            return request(app)
+                .get('/api/categories')
+                .expect(200)
+                .then(({ body }) => {
+                    expect(body.categories).toBeInstanceOf(Array);
+                    expect(body.categories).toHaveLength(4);
+                    body.categories.forEach((category) => {
+                        expect(category).toEqual(
+                            expect.objectContaining({
+                                slug: expect.any(String),
+                                description: expect.any(String)
+                            }));
+                    });
+                });
+        });
+    });
+
 });
+
 
 // GET /api/reviews/:review_id
 // PATCH /api/reviews/:review_id
--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const apiRouter = require('./routers/api.router');
-//const { handleCustomErrors, handlePsqlErrors, handleServerErrors } = require("./errors/errorHandling");
+const { handle404Errors, handleCustomErrors, handlePsqlErrors, handleServerErrors } = require("./errors/errorHandling");
 
 const app = express();
 app.use(express.json());
 
 app.use('/api', apiRouter);
 
-// app.all('*', (req, res) => {
-//   res.status(404).send({ msg: 'Route not found' });
-// });
-
-// app.use(handleCustomErrors);
-// app.use(handlePsqlErrors);
-// app.use(handleServerErrors);
+app.use(handle404Errors)
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
+app.use(handleServerErrors);
 
 module.exports = app;
