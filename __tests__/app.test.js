b81bc1e992c2335d12a6d3138b52290cbbf0bfbd
Iznara <ryanmcguire1991@gmail.com>
2021-11-30 12:39:58 +0000

    written happy path test for GET /api/categories endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,46 @@
+const request = require('supertest');
+
+const seed = require('../db/seeds/seed.js');
+const app = require('../app');
 const db = require('../db/connection.js');
 const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+
+describe('GET /api/categories', () => {
+    test('status:200, responds with an array of \'categories\' objects', () => {
+        return request(app)
+            .get('/api/categories')
+            .expect(200)
+            .then(({ body }) => {
+                expect(body.categories).toBeInstanceOf(Array);
+                expect(body.categories).toHaveLength(4);
+                body.categories.forEach((category) => {
+                    expect.objectContaining({
+                        slug: expect.any(String),
+                        description: expect.any(String)
+                    });
+                });
+            });
+    });
+});
+
+// GET /api/reviews/:review_id
+// PATCH /api/reviews/:review_id
+// GET /api/reviews
+// GET /api/reviews/:review_id/comments
+// POST /api/reviews/:review_id/comments
+// DELETE /api/comments/:comment_id
+// GET /api
+
+// 200 OK
+// 201 Created
+// 204 No Content
+// 400 Bad Request
+// 404 Not Found
+// 405 Method Not Allowed
+// 418 I'm a teapot
+// 422 Unprocessable Entity
+// 500 Internal Server Error
