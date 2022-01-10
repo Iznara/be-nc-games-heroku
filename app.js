a864acd046de232c1e0d8b73ed3c11e1db8eadc9
Iznara <ryanmcguire1991@gmail.com>
2022-01-10 18:41:13 +0000

    allow CORS - cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const apiRouter = require('./routers/api.router');
 const { handle404Errors, handleCustomErrors, handlePsqlErrors, handleServerErrors } = require("./errors/errorHandling");
 
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
